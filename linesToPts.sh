07ae48277d2e39538d406c29ca8fb18185c09d37
uashrani <ashra061@umn.edu>
2025-04-16 13:42:56 -0500

    get elevations of ditch endpoints

--- linesToPts.sh
+++ linesToPts.sh
@@ -8,15 +8,20 @@ combTable='ditchCombinations'     # distances between points and lines, can find
 combFile='ditchCombinations.txt'
 ptFile='ditchNodes.txt'
 
+# External link to data
+dem='mnDEM'
+
 # Extract start and end points from ditch lines
 v.to.points input=$vecLines output=$vecPoints use='node' --overwrite
 # Get xy coordinates of the points
 v.to.db map=$vecPoints layer=2 option='coor' columns='x','y' --overwrite
+# Get elevations of these points
+v.what.rast map=$vecPoints layer=2 raster=$dem column='elev'
 
 ### In addition to start and end points, we also want to know intersections with other ditches
 
 # Get distances from nodes to all nearby lines, and save in table
-v.distance -a from=$vecPoints from_layer=2 to=$vecLines dmax=1 upload='dist','cat' table=$combTable --overwrite
+v.distance -a from=$vecPoints from_layer=2 to=$vecLines dmax=1 upload='dist','cat','to_x','to_y' table=$combTable --overwrite
 # Export the table to a text file, and do the same for the ditch node attribute table
 db.select table=$combTable output=$combFile separator='comma' --overwrite
 v.db.select map=$vecPoints layer=2 format='csv' file=$ptFile --overwrite
