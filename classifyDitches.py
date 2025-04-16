462ac77640f0878befe9eceb5b6db58781723cd0
uashrani <ashra061@umn.edu>
2025-04-16 13:24:11 -0500

    determines junction in for loop

--- classifyDitches.py
+++ classifyDitches.py
@@ -9,22 +9,29 @@ ditchNodes = pd.read_csv(ptFile)
 
 ditchCombs = ditchCombs.rename(columns={'from_cat': 'fromPt', 'cat': 'toLine'})
 
-fromLines = []
+#fromLines = []
+ditchCombs['junction']=False
 
 for i in range(len(ditchCombs)):
     fromPt = ditchCombs['fromPt'].iloc[i]
 
     # Find the corresponding entry in ditchNodes with that point number
-    nodesEntry = ditchNodes[ditchNodes['cat']==fromPt]
+    nodesEntry = ditchNodes[ditchNodes['cat']==fromPt].iloc[0]
 
-    fromLine = nodesEntry['lcat'].iloc[0]
-    fromLines += [fromLine]
+    fromLine = nodesEntry['lcat']
+    #fromLines += [fromLine]
 
-ditchCombs['fromLine']=fromLines
-ditchCombs['junction']=(ditchCombs['fromLine']!=ditchCombs['toLine'])
+    if fromLine != ditchCombs['toLine'].iloc[i]:
+        ditchCombs.loc[i, 'junction'] = True
+
+#ditchCombs['fromLine']=fromLines
+#ditchCombs['junction']=(ditchCombs['fromLine']!=ditchCombs['toLine'])
 
 ditchJuncs = ditchCombs[ditchCombs['junction']==True]
 
 print(ditchJuncs)
+
+# Verify that each junction is the downstream end of the ditch
+
 
 
