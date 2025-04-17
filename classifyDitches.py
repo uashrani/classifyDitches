703f070db6942db4bf59cc77c18ad4a12e1798f0
uashrani <ashra061@umn.edu>
2025-04-17 14:30:10 -0500

    trying to combine back into Py script

--- classifyDitches.py
+++ classifyDitches.py
@@ -1,9 +1,29 @@
+import grass.script as gs
 import pandas as pd
+
+vecLines='drainage_centerlines'   # name of ditch layer in Grass, already imported
+vecPoints='ditch_nodes'            # name of start/end points, will be created from line data
+
+combTable='ditchCombinations'     # distances between points and lines, can find distances between every pair of ditches
 
 # Names of files exported from Grass
 combFile='ditchCombinations.txt'        # has distances from points to lines
 ptFile='ditchNodes.txt'                 # has point attributes like elevation and xy coords
 
+# External link to data
+dem='mnDEM'
+
+# Extract start and end points from ditch lines
+gs.run_command('v.to.points', input=vecLines, output=vecPoints, use='node', overwrite=True)
+# Get xy coordinates of the points
+gs.run_command('v.to.db', map=vecPoints, layer=2, option='coor', columns=['x', 'y'], overwrite=True)
+ 
+### In addition to start and end points, we also want to know intersections with other ditches
+ 
+# Get distances from nodes to all nearby lines, and save in table
+gs.run_command('v.distance', flags='a', _from=vecPoints, from_layer=2, to=vecLines, dmax=1, upload=['dist', 'cat'], table=combTable)
+
+### -------------------------------------------
 ditchCombs = pd.read_csv(combFile)
 ditchNodes = pd.read_csv(ptFile)
 
