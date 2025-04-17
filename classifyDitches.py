ad5302835a8a09e19b0500ff80a8f3d40dc5ed52
uashrani <ashra061@umn.edu>
2025-04-17 14:23:53 -0500

    check if junction is at downstream end

--- classifyDitches.py
+++ classifyDitches.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
 # Names of files exported from Grass
-combFile='ditchCombinations.txt'
-ptFile='ditchNodes.txt'
+combFile='ditchCombinations.txt'        # has distances from points to lines
+ptFile='ditchNodes.txt'                 # has point attributes like elevation and xy coords
 
 ditchCombs = pd.read_csv(combFile)
 ditchNodes = pd.read_csv(ptFile)
 
 ditchCombs = ditchCombs.rename(columns={'from_cat': 'fromPt', 'cat': 'toLine'})
+
+print(ditchNodes)
 
 #fromLines = []
 ditchCombs['junction']=False
@@ -24,14 +26,25 @@ for i in range(len(ditchCombs)):
     if fromLine != ditchCombs['toLine'].iloc[i]:
         ditchCombs.loc[i, 'junction'] = True
 
+        ### Verify that junction is downstream end of ditch
+        
+        # Get the points that lie along the same line
+        siblingPts = ditchNodes[ditchNodes['lcat']==fromLine]
+        minElev = min(siblingPts['elev'])
+        if nodesEntry['elev'] > minElev:
+            print(str(fromPt) + '\tJunction is not downstream end of incoming ditch')
+        else:
+            print(str(fromPt) + '\tMin elev of line ' + str(fromLine) + ' is ' + str(minElev))
+
+    #nodesEntry['elev']
+
 #ditchCombs['fromLine']=fromLines
 #ditchCombs['junction']=(ditchCombs['fromLine']!=ditchCombs['toLine'])
 
 ditchJuncs = ditchCombs[ditchCombs['junction']==True]
 
-print(ditchJuncs)
+#print(ditchJuncs)
 
-# Verify that each junction is the downstream end of the ditch
 
 
 
