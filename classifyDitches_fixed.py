6c462a4e4ba3b724d6b813fb70a42a59e65666c4
uashrani <ashra061@umn.edu>
2025-05-21 11:08:24 -0500

    organizing files

---
+++ classifyDitches_fixed.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  4 20:48:37 2025
+
+@author: swimm
+"""
+
+import pandas as pd
+import networkx as nx
+import numpy as np
+import grass.script as gs
+
+combFile = 'ditchCombinations.txt'
+startFile = 'ditchStartPts.txt'
+endFile = 'ditchEndPts.txt'
+linesFile = 'ditchLines.txt'
+
+ditchCombs = pd.read_csv(combFile)
+ditchStarts = pd.read_csv(startFile)
+ditchEnds = pd.read_csv(endFile)
+
+ditchCombs = ditchCombs.rename(columns={'from_cat': 'fromPt', 'cat': 'toPt'})
+
+def findOrder(lcat, ditchLines):
+    order = ditchLines.loc[ditchLines['cat']==lcat, 'order'].iloc[0]
+    
+    if order > 0:
+        return(order, ditchLines)
+    else:
+        parents = list(graph.predecessors(lcat))
+        parentOrders = []
+        for parent in parents:
+            if parent in list(ditchLines['cat']):
+                parentOrder, ditchLines = findOrder(parent, ditchLines)
+            
+                parentOrders += [parentOrder]            
+            
+        parentOrders = pd.Series(parentOrders)
+        
+        # Get the max stream order of the parents 
+        maxOrder = max(parentOrders)
+        nmax = len(parentOrders[parentOrders==maxOrder])
+        
+        if nmax > 1:
+            order = maxOrder + 1 
+        else:
+            order = maxOrder
+        
+        ditchLines.loc[ditchLines['cat']==lcat, 'order'] = order
+        
+        return(order, ditchLines)
+
+### In ditch combinations csv, find associated line for each point
+### and determine flow direction between ditches
+
+ditchCombs['fromLine']=0
+ditchCombs['toLine']=0
+
+for i in range(len(ditchCombs)):
+    # Read the current row in the "combinations" table
+    combEntry = ditchCombs.iloc[i]
+    fromPt, toPt = combEntry['fromPt'], combEntry['toPt']
+    
+    # Find the corresponding rows in the point attribute tables
+    # Remember: 'from' is downstream, 'to' is upstream
+    fromEntry = ditchEnds[ditchEnds['cat']==fromPt].iloc[0]
+    toEntry = ditchStarts[ditchStarts['cat']==toPt].iloc[0]
+    
+    # Get line numbers associated with points
+    fromLine, toLine = fromEntry['lcat'], toEntry['lcat']
+
+    if fromLine != toLine: 
+        ditchCombs.loc[i, 'fromLine'] = fromLine
+        ditchCombs.loc[i, 'toLine'] = toLine
+        
+# Go through all lines for upstream neighbors & stream order
+ditchLines = pd.read_csv(linesFile)
+ditchLines = ditchLines[ditchLines['len'] >= 1].reset_index(drop=True)
+
+ditchLines['parents']=''
+ditchLines['order']=0
+
+graph = nx.from_pandas_edgelist(ditchCombs, source='fromLine', target='toLine', create_using=nx.DiGraph)
+
+# First pass: note parents of each ditch
+for i in range(len(ditchLines)):
+    lcat = ditchLines['cat'].iloc[i]
+    
+    # Find upstream neighbors/parents
+    if graph.has_node(lcat):
+        parents = str(list(graph.predecessors(lcat)))
+    else:
+        parents = '[]'
+    
+    ditchLines.loc[i, 'parents'] = parents
+    
+ditchLines.loc[ditchLines['parents'] == '[]', 'order'] = 1
+
+file = 'linRegPts.txt'
+
+df = pd.read_csv(file) 
+df['elev']=df['elev'] / 100
+
+dfWithElevs = df[np.isnan(df['elev'])==False]
+
+lcats = sorted(set(dfWithElevs['lcat']))
+
+#gs.run_command('v.edit', map_='order1', type_='line', tool='create', overwrite=True)
+#gs.run_command('v.edit', map_='order2', type_='line', tool='create', overwrite=True)
+gs.run_command('v.db.addcolumn', map_='ditch_lines_renamed', columns='order int')
+
+for lcat in lcats:
+    print(lcat)
+    
+    if lcat in list(ditchLines['cat']):
+        order, ditchLines = findOrder(lcat, ditchLines)
+            
+        if order==1:
+            gs.run_command('v.edit', map_='order1', tool='copy', bgmap='ditch_lines_renamed', cat=lcat, overwrite=True)
+        if order==2: 
+            gs.run_command('v.edit', map_='order2', tool='copy', bgmap='ditch_lines_renamed', cat=lcat, overwrite=True)
+    
+        #gs.run_command('v.db.update', map_='ditch_lines_renamed', column='order', value=order, where="cat="+str(lcat))
+
+#ditchCombs.to_csv('final_' + combFile, index=False, sep='\t', float_format='%.3f')
--- ditchSlopes.py
+++ ditchSlopes.py
@@ -8,26 +8,29 @@ Created on Tue Apr 22 09:54:26 2025
 import pandas as pd
 import numpy as np
 import scipy as sp
+import grass.script as gs
 import math
 import matplotlib.pyplot as plt
 
 file = 'linRegPts.txt'
 
 df = pd.read_csv(file) 
+df['elev']=df['elev'] / 100
 
 dfWithElevs = df[np.isnan(df['elev'])==False]
 
 lcats = sorted(set(dfWithElevs['lcat']))
+#lcats = [35, 42, 126, 148, 631, 632]
 
-fig,axs=plt.subplots(4, 4, figsize=(20, 16))
-plt.subplots_adjust(wspace=0.3)
+fig,axs=plt.subplots(2, 3, figsize=(14, 6))
+plt.subplots_adjust(hspace=0.3)
 ax = axs.flat
 
 i=0
 for lcat in lcats:
     profilePts = dfWithElevs[dfWithElevs['lcat']==lcat]
     
-    if profilePts['along'].iloc[-1] > 0.01:    
+    if profilePts['along'].iloc[-1] > .1:    
         elev=profilePts['elev']
         along=profilePts['along']
         
@@ -45,35 +48,47 @@ for lcat in lcats:
         
         linreg = sp.stats.linregress(filtAlong, filtElev)    
         
-        ax[i].plot(along, elev, 'lightgray', ls='', marker='.') 
-        ax[i].plot(filtAlong, filtElev,'gray', ls='',marker='.')
-        ax[i].plot(along, linreg.slope * along + linreg.intercept, 'k')
+        # ax[i].plot(along, elev, 'lightsteelblue', ls='', marker='.') 
+        # ax[i].plot(filtAlong, filtElev,'xkcd:purplish brown', ls='',marker='.')
+        # ax[i].plot(along, linreg.slope * along + linreg.intercept, 'k')
         
-        ax[i].set_title('Ditch ' + str(lcat))
+        # ax[i].set_title('Ditch ' + str(lcat))
         
         annotation='m='+str(round(linreg.slope, 6)) + \
-            '\nr$^2$='+str(round(linreg.rvalue**2, 3)) + \
-                    '\nm_err='+str(round(linreg.stderr,6))
+            '\nr$^2$='+str(round(linreg.rvalue**2, 3)) # + \
+                #    '\nm_err='+str(round(linreg.stderr,6))
       
         if linreg.slope < 0:
-            xy=(.62, .75)
+            xy=(.62, .78) 
         else:
-            xy=(.03, .75)
+            xy=(.03, .78) # Changes plot display
+            # CHANGE IN FINAL VERSION
+            gs.run_command('v.edit', map_='ditch_lines_renamed', tool='flip', cats=lcat)
       
-        ax[i].annotate(annotation, xy=xy, xycoords='axes fraction', fontweight='bold', \
-                       bbox={'facecolor': 'wheat', 'alpha': 0.3})
+        #ax[i].annotate(annotation, xy=xy, xycoords='axes fraction', fontweight='bold', \
+        #               bbox={'facecolor': 'xkcd:light mint green', 'alpha': 0.3})
     
         i+=1
         
-fig, ax = plt.subplots(figsize=(10,8))
-profilePts = dfWithElevs[dfWithElevs['lcat']==27]
-adjustedDf = pd.read_csv('shiftedDitch27.txt')
+#ax[0].annotate('Along [m]', xy=(0.45, 0.03), xycoords='figure fraction', \
+               #fontweight='bold', fontsize=15)
+#ax[0].annotate('Elevation [m]', xy=(0.04, 0.4), xycoords='figure fraction', \
+               #fontweight='bold', fontsize=15, rotation=90)
+#fig.suptitle('Ditch Segment Long-Profiles', y=0.96, fontweight='bold', fontsize=16)        
 
-ax.plot(profilePts['along'], profilePts['elev'], 'gray', ls='', marker='.', label='original')
-ax.plot(adjustedDf['along'], adjustedDf['elev'], 'cornflowerblue', ls='', marker='.', label='adjusted')
 
-plt.legend()
 
-ax.set_xlabel('Along [m]')
-ax.set_ylabel('Elevation [m]')
-ax.set_title('Ditch 27')
+
+
+# fig, ax = plt.subplots(figsize=(10,8))
+# profilePts = dfWithElevs[dfWithElevs['lcat']==27]
+# adjustedDf = pd.read_csv('shiftedDitch27.txt')
+
+# ax.plot(profilePts['along'], profilePts['elev'], 'gray', ls='', marker='.', label='original')
+# ax.plot(adjustedDf['along'], adjustedDf['elev'], 'cornflowerblue', ls='', marker='.', label='adjusted')
+
+# plt.legend()
+
+# ax.set_xlabel('Along [m]')
+# ax.set_ylabel('Elevation [m]')
+# ax.set_title('Ditch 27')
