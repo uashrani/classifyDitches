8a57725ede6be4520f4765db71ef4d6382ffe6df
uashrani <ashra061@umn.edu>
2025-05-27 12:02:16 -0500

    just break lines at junctions, don't find flow directions or relationships yet

---
+++ splitJunctions.py
@@ -0,0 +1,82 @@
+import grass.script as gs
+import pandas as pd
+
+vecLines1='drainage_centerlines'   # name of ditch layer in Grass, already imported
+vecLines2='ditch_lines_nameless'
+vecLines3='ditch_lines_renamed'
+vecLines='ditch_lines_final'
+vecPoints1='ditch_nodes_old'  
+vecPoints='ditch_nodes'
+
+combTable='ditchCombinations'     # distances between points and lines, can find distances between every pair of ditches
+intersectTable='ditchIntersections'
+
+# Names of files to export from Grass
+combFile='ditchCombinations.txt'        # has distances from points to lines
+ptFile='ditchNodes.txt'                 # has point attributes like elevation and xy coords
+
+ptFileTemp='ditchNodesTemp.txt'
+intersectFileTemp='ditchIntersections.txt'
+
+# External link to data
+dem='mnDEM'
+### Function definitions----------------------------------------------------------------------------------
+
+def split_nodesIntersects(ptFile, intersectFile, linesLayer):
+    """ Takes two csv's, one containing nodes and one containing line intersections.
+    Creates a set with the xy coordinates along which to break the line vectors. 
+    Breaks lines along these xy coordinates. """
+
+    pts = pd.read_csv(ptFile)
+    intersects = pd.read_csv(intersectFile)
+    intersects.rename(columns={'to_x': 'x', 'to_y': 'y'}, inplace=True)
+
+    ptsXY=pts[['x', 'y']]
+    intersectsXY=intersects[['x', 'y']]
+    breakDf = pd.concat((ptsXY, intersectsXY))
+
+    for col in ['x', 'y']:
+        breakDf[col] = breakDf[col].round(2)
+
+    breakDf['tuple'] = '(' + breakDf['x'].astype('str') + ',' + breakDf['y'].astype('str') + ')'
+
+    tupleSet = set(breakDf['tuple'])
+
+    for elt in tupleSet:
+        elt = elt.strip('()')
+        x, y = tuple(map(float,elt.split(',')))
+
+        gs.run_command('v.edit', map=linesLayer, tool='break', coords=[x,y], threshold=1)
+
+### ------------------------------------------------------------------------------------------------------
+# Temporary: drop table because overwrite doesn't work
+gs.run_command('db.droptable', flags='f', table=intersectTable)
+
+# Get list of all nodes and their xy coordinates, will split lines at these points
+gs.run_command('v.to.points', input=vecLines1, output=vecPoints1, use='node', overwrite=True)
+gs.run_command('v.to.db', map=vecPoints1, layer=2, option='coor', columns=['x', 'y'], overwrite=True)
+# Export attribute table of these points
+gs.run_command('v.db.select', map=vecPoints1, layer=2, format='csv', file=ptFileTemp, overwrite=True)
+
+# Also get intersections between lines, since there is not always a node at the intersection
+gs.run_command('v.distance', flags='a', from_=vecLines1, to=vecLines1, dmax=1, \
+    upload=['to_x', 'to_y', 'cat'], table=intersectTable, overwrite=True)
+gs.run_command('db.select', table=intersectTable, separator='comma', output=intersectFileTemp, overwrite=True)
+
+split_nodesIntersects(ptFileTemp, intersectFileTemp, vecLines1)
+
+### When we split lines into segments, their category number didn't change (only feature ID did)
+### Create new attribute table so every segment has unique category number
+
+# Delete old category numbers and assign new category number to each segment
+gs.run_command('v.category', input=vecLines1, output=vecLines2, option='del', cat=-1, overwrite=True)
+gs.run_command('v.category', input=vecLines2, output=vecLines3, option='add', overwrite=True)
+
+# Disconnect from old attribute table and create new one
+gs.run_command('db.droptable', flags='f', table=vecLines3)
+gs.run_command('v.db.connect', flags='d', map=vecLines3, layer=1)
+gs.run_command('v.db.addtable', map=vecLines3)
+
+gs.run_command('v.to.db', map=vecLines3, option='length', columns=['len'])
+gs.run_command('v.db.droprow', input=vecLines3, where="len<0.01", output=vecLines, overwrite=True)
+
