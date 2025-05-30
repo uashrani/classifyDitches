dc1844c3e7084bf952a9b308e5f6e8ee3defc7da
uashrani <ashra061@umn.edu>
2025-05-30 17:26:34 -0500

    preserve original cat using v.category; find segments along same original line

--- splitJunctions.py
+++ splitJunctions.py
@@ -5,9 +5,12 @@ Author: Uma
 #%% Prerequisite modules, data, and folders
 
 import grass.script as gs
+import grass.grassdb.data as gdb
 import pandas as pd
+import numpy as np
+import networkx as nx
 
-vecLines1='drainage_centerlines'   # name of ditch layer in Grass, already imported
+vecLines0='drainage_centerlines'   # name of ditch layer in Grass, already imported
 
 tmpFiles = 'tempFiles/'
 
@@ -15,6 +18,7 @@ ditchPrefix='BRR'
 
 #%% Layers/files that will be created automatically
 
+vecLines1=vecLines0 + '_modifiable'
 vecLines2=ditchPrefix + '_lines_nameless'
 vecLines3=ditchPrefix + '_lines_renamed'
 vecLines=ditchPrefix + '_lines_final'
@@ -26,6 +30,12 @@ intersectTable = ditchPrefix + '_intersections'
 # Names of files to export from Grass
 ptFileTemp=tmpFiles + ditchPrefix + '_nodes.txt'
 intersectFileTemp=tmpFiles + ditchPrefix + '_intersections.txt'
+
+# Start and end points
+startNodes = ditchPrefix + '_starts'
+endNodes = ditchPrefix + '_ends'
+
+mergeFile=tmpFiles + 'mergeLines.txt'
 
 # Layers/files for the along profile
 profilePts=ditchPrefix + '_profilePts'  # GRASS layer
@@ -47,7 +57,7 @@ def split_nodesIntersects(ptFile, intersectFile, linesLayer):
     breakDf = pd.concat((ptsXY, intersectsXY))
 
     for col in ['x', 'y']:
-        breakDf[col] = breakDf[col].round(2)
+        breakDf[col] = breakDf[col].round(1)
 
     breakDf['tuple'] = '(' + breakDf['x'].astype('str') + ',' + breakDf['y'].astype('str') + ')'
 
@@ -64,45 +74,125 @@ def split_nodesIntersects(ptFile, intersectFile, linesLayer):
 # Temporary: drop table because overwrite doesn't work
 #gs.run_command('db.droptable', flags='f', table=intersectTable)
 
-gs.run_command('g.region', vector=vecLines1)
+gs.run_command('g.region', vector=vecLines0)
 
+if not gdb.map_exists(vecLines, 'vector'):
+    gs.run_command('g.copy', vector=[vecLines0, vecLines1])
+    
 # Get list of all nodes and their xy coordinates, will split lines at these points
-gs.run_command('v.to.points', input_=vecLines1, output=vecPoints1, use='node', overwrite=True)
-gs.run_command('v.to.db', map_=vecPoints1, layer=2, option='coor', columns=['x', 'y'], overwrite=True)
-# Export attribute table of these points
-gs.run_command('v.db.select', map_=vecPoints1, layer=2, format_='csv', file=ptFileTemp, overwrite=True)
+    gs.run_command('v.to.points', input_=vecLines1, output=vecPoints1, use='node', overwrite=True)
+    gs.run_command('v.to.db', map_=vecPoints1, layer=2, option='coor', columns=['x', 'y'], overwrite=True)
+    # Export attribute table of these points
+    gs.run_command('v.db.select', map_=vecPoints1, layer=2, format_='csv', file=ptFileTemp, overwrite=True)
+    
+    # Also get intersections between lines, since there is not always a node at the intersection
+    gs.run_command('v.distance', flags='a', from_=vecLines1, to=vecLines1, dmax=1, \
+        upload=['to_x', 'to_y', 'cat'], table=intersectTable, overwrite=True)
+    gs.run_command('db.select', table=intersectTable, separator='comma', output=intersectFileTemp, overwrite=True)
+    
+    split_nodesIntersects(ptFileTemp, intersectFileTemp, vecLines1)
+    
+    ### When we split lines into segments, their category number didn't change (only feature ID did)
+    ### Create new attribute table so every segment has unique category number
+    # Delete old category numbers and assign new category number to each segment
+    gs.run_command('v.category', flags='t', input_=vecLines1, output=vecLines2, option='del', cat=-1, overwrite=True)
+    gs.run_command('v.category', input_=vecLines2, output=vecLines3, option='add', overwrite=True)
+    
+    # Disconnect from old attribute table and create new one
+    gs.run_command('v.db.connect', flags='d', map_=vecLines3, layer=1)
+    gs.run_command('v.db.addtable', map_=vecLines3)
 
-# Also get intersections between lines, since there is not always a node at the intersection
-gs.run_command('v.distance', flags='a', from_=vecLines1, to=vecLines1, dmax=1, \
-    upload=['to_x', 'to_y', 'cat'], table=intersectTable, overwrite=True)
-gs.run_command('db.select', table=intersectTable, separator='comma', output=intersectFileTemp, overwrite=True)
+    gs.run_command('v.to.db', map_=vecLines3, option='length', columns=['len'])
+    gs.run_command('v.db.droprow', input_=vecLines3, where="len < 0.1", output=vecLines, overwrite=True)
 
-split_nodesIntersects(ptFileTemp, intersectFileTemp, vecLines1)
 
-### When we split lines into segments, their category number didn't change (only feature ID did)
-### Create new attribute table so every segment has unique category number
+## Next create layers for start and end points
+if not gdb.map_exists(endNodes, 'vector'):
+    gs.run_command('v.to.points', input_=vecLines, output=startNodes, use='start', overwrite=True)
+    gs.run_command('v.to.points', input_=vecLines, output=endNodes, use='end', overwrite=True)
+    # Find which segments are connected
+    gs.run_command('v.db.addcolumn', map_=endNodes, layer=2, columns=['to_lcat int'])
+    gs.run_command('v.distance', from_=endNodes, to=startNodes, from_layer=2, to_layer=2, \
+                    dmax=1, upload='to_attr', to_column='lcat', \
+                        column='to_lcat', overwrite=True)
+    gs.run_command('v.db.select', map_=endNodes, layer=2, format_='csv', file=mergeFile, overwrite=True)
 
-# Delete old category numbers and assign new category number to each segment
-gs.run_command('v.category', flags='t', input_=vecLines1, output=vecLines2, option='del', cat=-1, overwrite=True)
-gs.run_command('v.category', input_=vecLines2, output=vecLines3, option='add', overwrite=True)
-
-# Disconnect from old attribute table and create new one
-gs.run_command('v.db.connect', flags='d', map_=vecLines3, layer=1)
-gs.run_command('v.db.addtable', map_=vecLines3)
-
-gs.run_command('v.to.db', map_=vecLines3, option='length', columns=['len'])
-#gs.run_command('v.db.droprow', input_=vecLines3, where="len<0.01", output=vecLines, overwrite=True)
-
+dfEnds=pd.read_csv(mergeFile)
+dfEnds=dfEnds[(dfEnds['lcat']!=dfEnds['to_lcat']) & (np.isnan(dfEnds['to_lcat'])==False)].reset_index(drop=True)
 # Keep track of original category numbers
-gs.run_command('v.db.addcolumn', map_=vecLines3, columns='orig_cat int')
-gs.run_command('v.distance', from_=vecLines3, to=vecLines1, upload='cat', column='orig_cat')
+orig_cats = gs.read_command('v.category', input_=vecLines1, option='print')
+ls_orig_cats = orig_cats.split('\r\n')
+ls_orig_cats = pd.Series(ls_orig_cats[:-1]).astype('int')
+fIDs = np.arange(1, len(ls_orig_cats)+1)
 
-### Next create layers for start and end points, and find connections where 1 lcat --> 1 lcat and both are from same original line
+dfOrig = pd.DataFrame({'cat': fIDs, 'orig_cat': ls_orig_cats})
+
+#dfOrig.to_csv('testView.txt', index=False)
+
+from_cats, to_cats=[], []
+
+# Get connections b/w segments that were originally part of same line
+for i in range(len(dfEnds)):
+    row=dfEnds.iloc[i]
+    f_cat, t_cat=row['lcat'], row['to_lcat']
+    
+    f_orig=dfOrig['orig_cat'][dfOrig['cat']==f_cat].iloc[0]
+    t_orig=dfOrig['orig_cat'][dfOrig['cat']==t_cat].iloc[0]
+    
+    if f_orig==t_orig: 
+        from_cats+=[f_cat]
+        to_cats+=[t_cat]
+        
+mergedLines=pd.DataFrame({'from':from_cats, 'to':to_cats})
+
+graph = nx.from_pandas_edgelist(mergedLines, source='from', target='to', create_using=nx.DiGraph)  
+
+dfOrig['nPrev']=0
+dfOrig['nNext']=0
+
+for i in range(len(dfOrig)):
+    lcat=dfOrig['cat'].iloc[i]
+    
+    if graph.has_node(lcat):
+        nPrev = len(list(graph.predecessors(lcat)))
+        nNext = len(list(graph.successors(lcat)))
+        
+        dfOrig.loc[i, 'nPrev'] = nPrev
+        dfOrig.loc[i, 'nNext'] = nNext
+       
+# To merge a segment, we want the number of its successors and predecessors to be 1
+singleChains=dfOrig[(dfOrig['nPrev']<=1) & (dfOrig['nNext']<=1) & ((dfOrig['nPrev'] + dfOrig['nNext']) > 0)]
+# Start at roots
+roots=singleChains[singleChains['nPrev']==0]
+
+for i in range(len(roots)):
+    rootLcat=roots['cat'].iloc[i]
+    
+    chain=[rootLcat]
+    nextLcats=list(graph.successors(rootLcat))
+    if len(nextLcats) > 0: 
+        nextLcat=nextLcats[0] 
+    else: 
+        nextLcat=0
+    
+    while nextLcat in list(singleChains['cat']):
+        chain+=[nextLcat]
+        
+        nextLcats=list(graph.successors(nextLcat))
+        if len(nextLcats) > 0: 
+            nextLcat=nextLcats[0]
+        else: 
+            nextLcat=0
+        
+    print(chain)
+        
+
+
 
 
 
 ### Get points spaced 1m apart along the new lines, will be used for transects
-gs.run_command('v.to.points', input_=vecLines3, output=profilePts, dmax=1)
-gs.run_command('v.to.db', map_=profilePts, layer=2, option='coor', columns=['x', 'y'])
-gs.run_command('v.db.select', map_=profilePts, layer=2, format_='csv', file=alongFile, overwrite=True)
+# gs.run_command('v.to.points', input_=vecLines3, output=profilePts, dmax=1)
+# gs.run_command('v.to.db', map_=profilePts, layer=2, option='coor', columns=['x', 'y'])
+# gs.run_command('v.db.select', map_=profilePts, layer=2, format_='csv', file=alongFile, overwrite=True)
 
