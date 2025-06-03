56da2a862c0ab48b878b30e1bd0de74534279d1d
uashrani <ashra061@umn.edu>
2025-06-03 17:16:55 -0500

    changed conditions for segments to be part of one chain

--- splitJunctions.py
+++ splitJunctions.py
@@ -21,7 +21,7 @@ ditchPrefix='BRR'
 vecLines1=vecLines0 + '_modifiable'
 vecLines2=ditchPrefix + '_lines_nameless'
 vecLines3=ditchPrefix + '_lines_renamed'
-vecLines=ditchPrefix + '_lines_final'
+vecLines=ditchPrefix + '_lines_filtered'
 
 vecPoints1=ditchPrefix + '_nodes_old'  
 
@@ -34,6 +34,7 @@ intersectFileTemp=tmpFiles + ditchPrefix + '_intersections.txt'
 # Start and end points
 startNodes = ditchPrefix + '_starts'
 endNodes = ditchPrefix + '_ends'
+origTable = ditchPrefix + '_origConnections'
 
 mergeFile=tmpFiles + 'mergeLines.txt'
 chainFile= tmpFiles + ditchPrefix + '_streamChains.txt'
@@ -106,20 +107,20 @@ if not gdb.map_exists(vecLines, 'vector'):
     gs.run_command('v.to.db', map_=vecLines3, option='length', columns=['len'])
     gs.run_command('v.db.droprow', input_=vecLines3, where="len < 0.1", output=vecLines, overwrite=True)
 
-
-## Next create layers for start and end points
+### Find which segments originally connected to each other (even if not actual flow dir)
 if not gdb.map_exists(endNodes, 'vector'):
+    # Next create layers for start and end points
     gs.run_command('v.to.points', input_=vecLines, output=startNodes, use='start', overwrite=True)
     gs.run_command('v.to.points', input_=vecLines, output=endNodes, use='end', overwrite=True)
     # Find which segments are connected
-    gs.run_command('v.db.addcolumn', map_=endNodes, layer=2, columns=['to_lcat int'])
-    gs.run_command('v.distance', from_=endNodes, to=startNodes, from_layer=2, to_layer=2, \
-                    dmax=1, upload='to_attr', to_column='lcat', \
-                        column='to_lcat', overwrite=True)
-    gs.run_command('v.db.select', map_=endNodes, layer=2, format_='csv', file=mergeFile, overwrite=True)
+    gs.run_command('v.distance', flags='a', from_=endNodes, to=startNodes, from_layer=1, to_layer=2, \
+                    dmax=0.2, upload='to_attr', to_column='lcat', \
+                        column='to_lcat', table= origTable, overwrite=True)
+    gs.run_command('db.select', table=origTable, separator='comma', output=mergeFile, overwrite=True)
 
 dfEnds=pd.read_csv(mergeFile)
-dfEnds=dfEnds[(dfEnds['lcat']!=dfEnds['to_lcat']) & (np.isnan(dfEnds['to_lcat'])==False)].reset_index(drop=True)
+dfEnds=dfEnds[dfEnds['from_cat']!=dfEnds['to_lcat']].reset_index(drop=True)
+
 # Keep track of original category numbers
 orig_cats = gs.read_command('v.category', input_=vecLines1, option='print')
 ls_orig_cats = orig_cats.split('\r\n')
@@ -128,68 +129,71 @@ fIDs = np.arange(1, len(ls_orig_cats)+1)
 
 dfOrig = pd.DataFrame({'cat': fIDs, 'orig_cat': ls_orig_cats})
 
+### We have a table showing all connections, 
+### but we narrow this down to connections b/w segments that had same original cat
 from_cats, to_cats=[], []
 
-# Get connections b/w segments that were originally part of same line
 for i in range(len(dfEnds)):
     row=dfEnds.iloc[i]
-    f_cat, t_cat=row['lcat'], row['to_lcat']
     
+    # These are their cat numbers in the split layer
+    f_cat, t_cat=row['from_cat'], row['to_lcat']
+    
+    # These are their cat numbers from the original layer (unbroken)
     f_orig=dfOrig['orig_cat'][dfOrig['cat']==f_cat].iloc[0]
     t_orig=dfOrig['orig_cat'][dfOrig['cat']==t_cat].iloc[0]
     
+    # If they came from the same original cat, add them to the to/from columns
     if f_orig==t_orig: 
         from_cats+=[f_cat]
         to_cats+=[t_cat]
         
+# Create a directed graph of segments that came from the same original lines
 mergedLines=pd.DataFrame({'from':from_cats, 'to':to_cats})
-
 graph = nx.from_pandas_edgelist(mergedLines, source='from', target='to', create_using=nx.DiGraph)  
 
-dfOrig['nPrev']=0
-dfOrig['nNext']=0
-
-for i in range(len(dfOrig)):
-    lcat=dfOrig['cat'].iloc[i]
-    
-    if graph.has_node(lcat):
-        nPrev = len(list(graph.predecessors(lcat)))
-        nNext = len(list(graph.successors(lcat)))
-        
-        dfOrig.loc[i, 'nPrev'] = nPrev
-        dfOrig.loc[i, 'nNext'] = nNext
-       
-# To merge a segment, we want the number of its successors and predecessors to be 1
-singleChains=dfOrig[(dfOrig['nPrev']<=1) & (dfOrig['nNext']<=1) & ((dfOrig['nPrev'] + dfOrig['nNext']) > 0)]
-# Start at roots
-rootLcats=singleChains['cat'][singleChains['nPrev']==0]
-
+### We will construct chains of segments, and keep track of the first in the chain
+### Chains should have 1 segment feeding into 1, & shouldn't have forks/branches
 chainCol = []
-rootCol = []
 
 for lcat in fIDs:
-    if graph.has_node(lcat)==False or len(list(graph.predecessors(lcat))) == 0:
-           chain=[int(lcat)]
-    else: 
-        chain=[]
-    if lcat in list(rootLcats):
+    # If a segment isn't in the directed graph, it forms its own chain
+    if graph.has_node(lcat)==False: 
+        chain=[int(lcat)]
+        
+    # If it has exactly one predecessor, it's part of another chain
+    elif len(list(graph.predecessors(lcat))) == 1:
+        prevLcats = list(graph.predecessors(lcat))
+        prevLcat = prevLcats[0]
+        
+        # But check that it doesn't have siblings
+        if len(list(graph.successors(prevLcat))) == 1:
+            chain=[]
+        else:
+            chain=[int(lcat)]
+        
+    # If it has 0 or 2+ predecessors, start a new chain here
+    else:
+        chain=[int(lcat)]
         
         nextLcats=list(graph.successors(lcat))
         
-        if len(nextLcats) > 0: 
-            nextLcat=nextLcats[0] 
-        else: 
-            nextLcat=0
+        # If a segment has 0 or 2+ successors, end the chain here
+        if len(nextLcats) != 1:
+            nextLcat=0 
+        else:
+            nextLcat=nextLcats[0]
         
-        while nextLcat in list(singleChains['cat']):
+        # Check to make sure the successor isn't receiving flow from another segment
+        while nextLcat != 0 and len(list(graph.predecessors(nextLcat)))==1:
             chain+=[int(nextLcat)]
             
-            nextLcats=list(graph.successors(nextLcat))
-            if len(nextLcats) > 0: 
-                nextLcat=nextLcats[0]
-            else: 
+            nextLcats = list(graph.successors(nextLcat))
+            if len(nextLcats) != 1:
                 nextLcat=0
-            
+            else:
+                nextLcat=nextLcats[0]
+               
     chainCol += [str(chain)]
     
 chainDf = pd.DataFrame({'root': fIDs, 'chain': chainCol})
