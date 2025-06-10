f700d282464caa5cccaaf99874721095e9f3ed20
uashrani <ashra061@umn.edu>
2025-06-10 10:47:43 -0500

    change cross-section width; shift points in culvert region by same distance as points outside of culvert region

--- ditchCrossSects.py
+++ ditchCrossSects.py
@@ -27,7 +27,7 @@ culvertDefFile = tmpFiles + ditchPrefix + '_culvertPtDefs.txt'
 n, s, e, w = 5202318, 5191400, 220687, 212912   # test region 2
 
 # How far to take the profile on each side, in m
-halfDist = 7.5    # slightly smaller than the null width
+halfDist = 15    # slightly smaller than the null width
 #%% Layers/files that will be created automatically
 
 lineDefFile= tmpFiles + hucPrefix + '_shiftedLineDefs.txt'
@@ -58,7 +58,8 @@ lcats=sorted(set(dfInRegion['lcat']))
 culvertPts = pd.read_csv(culvertDefFile, names=['x', 'y', 'buffer'])
 
 # Create empty vector map for new lines, and empty file to add coords
-newPtsDf = pd.DataFrame({'lcat': [], 'x': [], 'y': []})
+newPtsDf = pd.DataFrame({'lcat': [], 'x': [], 'y': [], 'across': [], \
+                         'x1': [], 'y1': [], 'cos': [], 'sin': [], 'culvert': []})
 gs.run_command('v.edit', map_=newLine, type_='line', tool='create', overwrite=True)
 
 for lcat in lcats:
@@ -75,56 +76,87 @@ for lcat in lcats:
     cosines=np.cos(angles)
     
     # Get the midpoints of all 1-m line segments
-    x_m = (x[1:].reset_index(drop=True) +x[:-1].reset_index(drop=True)) / 2
-    y_m = (y[1:].reset_index(drop=True) +y[:-1].reset_index(drop=True)) / 2
+    x_ms = (x[1:].reset_index(drop=True) +x[:-1].reset_index(drop=True)) / 2
+    y_ms = (y[1:].reset_index(drop=True) +y[:-1].reset_index(drop=True)) / 2
     
-    trX1 = x_m - halfDist*cosines
-    trX2 = x_m + halfDist*cosines
-    trY1 = y_m - halfDist*sines
-    trY2 = y_m + halfDist*sines
+    trX1 = x_ms - halfDist*cosines
+    trX2 = x_ms + halfDist*cosines
+    trY1 = y_ms - halfDist*sines
+    trY2 = y_ms + halfDist*sines
     
-    ncoords = len(x_m)
+    ncoords = len(x_ms)
     coordsToAdd = list(range(0,ncoords,10))+[ncoords-1]  # go every 10m but include end
+    
+    prevAcross = halfDist
     
     # Get profile across these endpoints
     for i in coordsToAdd:
         
+        # Assume the point is in a culvert, change these variables later if it's not
+        across = prevAcross
+        culvert=True
+        
+        x_m, y_m = x_ms.iloc[i], y_ms.iloc[i]
         x1,y1,x2,y2=trX1.iloc[i],trY1.iloc[i],trX2.iloc[i],trY2.iloc[i]
         cos,sin=cosines[i], sines[i]
         
-        gs.run_command('r.profile', input_=dem, output=tmpFile, \
-                        coordinates=[x1,y1,x2,y2], overwrite=True)
-    
-        profile=pd.read_csv(tmpFile, sep='\s+', names=['across', 'elev'], na_values='*')
-        crossElev=profile['elev']
-        minElev=np.min(crossElev)
-        
-        if np.isnan(minElev):
-            newX, newY = x_m.iloc[i], y_m.iloc[i]
-        else:
-            minAcross = profile[crossElev==minElev].iloc[0]
-            across=minAcross['across']
-            
-            newX, newY = x1 + across*cos, y1 + across*sin
-            
-        # Check if it's near a culvert
-        culvertPts['distToPt']=np.sqrt((culvertPts['x']-newX)**2+(culvertPts['y']-newY)**2)
+        # Check if the point is near a culvert
+        culvertPts['distToPt']=np.sqrt((culvertPts['x']-x_m)**2+(culvertPts['y']-y_m)**2)
         culvertsNearby =  culvertPts[culvertPts['distToPt'] < culvertPts['buffer']]
         
-        # Only include it in the line if it's not near a culvert
+        # If it's not near a culvert, find the minimum elevation along transect
         if len(culvertsNearby) == 0:     
-            newRow = pd.DataFrame({'lcat': [lcat], 'x': [newX], 'y': [newY]})
-            newPtsDf = pd.concat((newPtsDf, newRow))
+            culvert=False
+
+            gs.run_command('r.profile', input_=dem, output=tmpFile, \
+                            coordinates=[x1,y1,x2,y2], overwrite=True)
+        
+            profile=pd.read_csv(tmpFile, sep='\s+', names=['across', 'elev'], na_values='*')
+            crossElev=profile['elev']
+            minElev=np.min(crossElev)
+            
+            if np.isnan(minElev):
+                newX, newY = x_m, y_m
+            else:
+                # across gets changed if 
+                minAcross = profile[crossElev==minElev].iloc[0]
+                across=minAcross['across']
+        
+        prevAcross = across
+            
+        newX, newY = x1 + across*cos, y1 + across*sin
+        
+        newRow = pd.DataFrame({'lcat': [lcat], 'x': [newX], 'y': [newY], \
+                               'across': [across], 'x1': [x1], 'y1': [y1], \
+                                   'cos': [cos], 'sin': [sin], 'culvert': [culvert]})
+        newPtsDf = pd.concat((newPtsDf, newRow))
             
 # Now write to a file since we know how many points are in each line
 fLine=open(lineDefFile, 'a')
 
 for lcat in lcats:
-    linePts = newPtsDf[newPtsDf['lcat']==lcat]
+    linePts = newPtsDf[newPtsDf['lcat']==lcat].reset_index(drop=True)
     nPts = len(linePts)
     
     fLine.write('L  ' + str(nPts) + ' 1\n')
     
+    ### Fill in any start points that were in a culvert
+    
+    # Get the index of the first non-culvert point, edit preceding across values
+    nonCuls = linePts.index[linePts['culvert']==False]
+    if len(nonCuls) > 0:
+        nonCul = nonCuls[0]
+        linePts.loc[:(nonCul-1), 'across'] = linePts['across'].iloc[nonCul]
+        
+        # These are just for the start points in a culvert
+        x1s, y1s = linePts['x1'].iloc[:nonCul], linePts['y1'].iloc[:nonCul]
+        acrosses = linePts['across'].iloc[:nonCul]
+        cosinez, sinez = linePts['cos'].iloc[:nonCul], linePts['sin'].iloc[:nonCul]
+        
+        # Update shifted x and y values
+        linePts.loc[:(nonCul-1), 'x'] =  x1s + acrosses*cosinez
+        linePts.loc[:(nonCul-1), 'y'] =  y1s + acrosses*sinez
+
     for i in range(nPts):
         newX, newY = linePts['x'].iloc[i], linePts['y'].iloc[i]
         fLine.write(' ' + str(newX) + ' ' + str(newY) + '\n')
