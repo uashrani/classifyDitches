433c545f84dc461a699568a782e675631526c7db
uashrani <ashra061@umn.edu>
2025-06-11 11:29:12 -0500

    create stream chain file every time (temporary)

--- splitJunctions.py
+++ splitJunctions.py
@@ -119,88 +119,88 @@ if not gdb.map_exists(endNodes, 'vector'):
     
     gs.run_command('db.select', table=connectTable, separator='comma', output=connectFile, overwrite=True)
     
-    #%% Find which segments originally connected to each other (even if not actual flow dir)
+#%% Find which segments originally connected to each other (even if not actual flow dir)
 
-    dfEnds=pd.read_csv(connectFile)
-    dfEnds=dfEnds[dfEnds['from_cat']!=dfEnds['to_lcat']].reset_index(drop=True)
+dfEnds=pd.read_csv(connectFile)
+dfEnds=dfEnds[dfEnds['from_cat']!=dfEnds['to_lcat']].reset_index(drop=True)
+
+# Keep track of original category numbers
+orig_cats = gs.read_command('v.category', input_=vecLines1, option='print')
+ls_orig_cats = orig_cats.split('\r\n')
+ls_orig_cats = pd.Series(ls_orig_cats[:-1]).astype('int')
+fIDs = np.arange(1, len(ls_orig_cats)+1)
+
+dfOrig = pd.DataFrame({'cat': fIDs, 'orig_cat': ls_orig_cats})
+
+### We have a table showing all connections, 
+### but we narrow this down to connections b/w segments that had same original cat
+from_cats, to_cats=[], []
+
+for i in range(len(dfEnds)):
+    row=dfEnds.iloc[i]
     
-    # Keep track of original category numbers
-    orig_cats = gs.read_command('v.category', input_=vecLines1, option='print')
-    ls_orig_cats = orig_cats.split('\r\n')
-    ls_orig_cats = pd.Series(ls_orig_cats[:-1]).astype('int')
-    fIDs = np.arange(1, len(ls_orig_cats)+1)
+    # These are their cat numbers in the split layer
+    f_cat, t_cat=row['from_cat'], row['to_lcat']
     
-    dfOrig = pd.DataFrame({'cat': fIDs, 'orig_cat': ls_orig_cats})
+    # These are their cat numbers from the original layer (unbroken)
+    f_orig=dfOrig['orig_cat'][dfOrig['cat']==f_cat].iloc[0]
+    t_orig=dfOrig['orig_cat'][dfOrig['cat']==t_cat].iloc[0]
     
-    ### We have a table showing all connections, 
-    ### but we narrow this down to connections b/w segments that had same original cat
-    from_cats, to_cats=[], []
-    
-    for i in range(len(dfEnds)):
-        row=dfEnds.iloc[i]
+    # If they came from the same original cat, add them to the to/from columns
+    if f_orig==t_orig: 
+        from_cats+=[f_cat]
+        to_cats+=[t_cat]
         
-        # These are their cat numbers in the split layer
-        f_cat, t_cat=row['from_cat'], row['to_lcat']
+# Create a directed graph of segments that came from the same original lines
+mergedLines=pd.DataFrame({'from':from_cats, 'to':to_cats})
+graph = nx.from_pandas_edgelist(mergedLines, source='from', target='to', create_using=nx.DiGraph)  
+
+### We will construct chains of segments, and keep track of the first in the chain
+### Chains should have 1 segment feeding into 1, & shouldn't have forks/branches
+chainDf = pd.DataFrame({'root': fIDs})
+chainDf['chain']=''
+
+for lcat in fIDs:
+    # If a segment isn't in the directed graph, it forms its own chain
+    if graph.has_node(lcat)==False: 
+        chain=[int(lcat)]
         
-        # These are their cat numbers from the original layer (unbroken)
-        f_orig=dfOrig['orig_cat'][dfOrig['cat']==f_cat].iloc[0]
-        t_orig=dfOrig['orig_cat'][dfOrig['cat']==t_cat].iloc[0]
+    # If it has exactly one predecessor, it's part of another chain
+    elif len(list(graph.predecessors(lcat))) == 1:
+        prevLcats = list(graph.predecessors(lcat))
+        prevLcat = prevLcats[0]
         
-        # If they came from the same original cat, add them to the to/from columns
-        if f_orig==t_orig: 
-            from_cats+=[f_cat]
-            to_cats+=[t_cat]
-            
-    # Create a directed graph of segments that came from the same original lines
-    mergedLines=pd.DataFrame({'from':from_cats, 'to':to_cats})
-    graph = nx.from_pandas_edgelist(mergedLines, source='from', target='to', create_using=nx.DiGraph)  
-    
-    ### We will construct chains of segments, and keep track of the first in the chain
-    ### Chains should have 1 segment feeding into 1, & shouldn't have forks/branches
-    chainDf = pd.DataFrame({'root': fIDs})
-    chainDf['chain']=''
-    
-    for lcat in fIDs:
-        # If a segment isn't in the directed graph, it forms its own chain
-        if graph.has_node(lcat)==False: 
-            chain=[int(lcat)]
-            
-        # If it has exactly one predecessor, it's part of another chain
-        elif len(list(graph.predecessors(lcat))) == 1:
-            prevLcats = list(graph.predecessors(lcat))
-            prevLcat = prevLcats[0]
-            
-            # But check that it doesn't have siblings
-            if len(list(graph.successors(prevLcat))) == 1:
-                chain=[]
-            else:
-                chain=[int(lcat)]
-            
-        # If it has 0 or 2+ predecessors, start a new chain here
+        # But check that it doesn't have siblings
+        if len(list(graph.successors(prevLcat))) == 1:
+            chain=[]
         else:
             chain=[int(lcat)]
+        
+    # If it has 0 or 2+ predecessors, start a new chain here
+    else:
+        chain=[int(lcat)]
+        
+        nextLcats=list(graph.successors(lcat))
+        
+        # If a segment has 0 or 2+ successors, end the chain here
+        if len(nextLcats) != 1:
+            nextLcat=0 
+        else:
+            nextLcat=nextLcats[0]
+        
+        # Check to make sure the successor isn't receiving flow from another segment
+        while nextLcat != 0 and len(list(graph.predecessors(nextLcat)))==1:
+            chain+=[int(nextLcat)]
             
-            nextLcats=list(graph.successors(lcat))
-            
-            # If a segment has 0 or 2+ successors, end the chain here
+            nextLcats = list(graph.successors(nextLcat))
             if len(nextLcats) != 1:
-                nextLcat=0 
+                nextLcat=0
             else:
                 nextLcat=nextLcats[0]
-            
-            # Check to make sure the successor isn't receiving flow from another segment
-            while nextLcat != 0 and len(list(graph.predecessors(nextLcat)))==1:
-                chain+=[int(nextLcat)]
                 
-                nextLcats = list(graph.successors(nextLcat))
-                if len(nextLcats) != 1:
-                    nextLcat=0
-                else:
-                    nextLcat=nextLcats[0]
-                    
-        chainDf.loc[lcat-1, 'chain']=str(chain)
-        
-    chainDf.to_csv(chainFile, index=False)
+    chainDf.loc[lcat-1, 'chain']=str(chain)
+    
+chainDf.to_csv(chainFile, index=False)
 
 ### Get points spaced 1m apart along the new lines
 ### Will be used to take transects and check for duplicates
