8faf9a5e16222ae4c90e9b9614afff1022611f29
uashrani <ashra061@umn.edu>
2025-07-01 15:49:37 -0500

    converted program into function

---
+++ backup_copies/removeCulverts.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 21 11:03:19 2025
+
+@author: Uma
+"""
+
+#%% Prerequisite modules, data, and folders
+
+import grass.script as gs
+import grass.grassdb.data as gdb
+
+# Folder names
+tmpFiles = 'tempFiles/'
+hucPrefix='testDEM3' # use for operations that need the DEM
+ditchPrefix='BRR' # use for operations involving the entire ditch layer
+
+# We need ditch vector data, culvert vector data, and elevation raster data
+ditchesCropped = hucPrefix + '_shiftedDitches'  # only covers extent of DEM
+dem = 'testDEM3'
+
+culvertBuffers = ditchPrefix + '_culvertBuffers'  # vector layer containing circles around the culvert points
+
+#%% Layers/files that will be created automatically
+culvertLines = hucPrefix + '_culvertLines'    # segment of ditch that passes through culvert
+culvertEndpts = hucPrefix + '_culvertEndpoints' 
+
+culvertMask = hucPrefix + '_culvertMask'
+
+culvertRaster = hucPrefix + '_culvertSurf'
+
+demBurned = hucPrefix + '_burned'
+demNull = hucPrefix + '_wNulls'
+
+newPts = hucPrefix + '_shiftedVertices'
+newElevFile = tmpFiles + hucPrefix + '_elevProfile_shiftedDitches.txt'
+       
+#%% Need DEM for this part
+
+if not gdb.map_exists(demNull, 'raster'):
+    gs.run_command('g.region', raster=dem)
+    
+    # Then find where these buffers intersect the ditch lines
+    gs.run_command('v.overlay', ainput=ditchesCropped, atype='line', binput=culvertBuffers, \
+                    operator='and', output=culvertLines)
+    
+    # First find the endpoints of each culvert segment
+    gs.run_command('v.to.points', input_=culvertLines, output=culvertEndpts, use='node', overwrite=True)
+    
+    # Also, we need a narrow mask (for burning drainage), only in regions where culverts are
+    gs.run_command('v.buffer', flags='c', input_=culvertLines, type_='line', output=culvertMask, \
+                    distance=3) 
+    # Above is a vector, but we need a raster mask
+    gs.run_command('v.to.rast', input_=culvertMask, type_='area', output=culvertMask, use='value')
+        
+    ### Create interpolated surfaces where the culvert regions are
+    # Get elevation values at endpoints of culvert segments
+    gs.run_command('v.what.rast', map_=culvertEndpts, raster=dem, column='elev', layer=2, overwrite=True)
+    # Interpolate a surface from these points
+    gs.run_command('v.surf.rst', input_=culvertEndpts, zcolumn='elev', \
+                    elevation=culvertRaster, mask=culvertMask, layer=2)
+    # Now patch the interpolated section with the original DEM,
+    # using the interpolated part as the primary raster
+    gs.run_command('r.patch', input_=[culvertRaster,dem], output=demBurned)
+    
+    ### Create null regions where the culvert regions are
+    expr=demNull + '=if(isnull('+ culvertMask+ '),' + dem + ', 0)'
+    gs.run_command('r.mapcalc', expression=expr)
+    gs.run_command('r.null', map_=demNull, setnull=0)
+    
+    gs.run_command('v.to.points', input_=ditchesCropped, dmax=1, output=newPts)
+    gs.run_command('v.to.db', map_=newPts, layer=2, option='coor', columns=['x', 'y'])
+    gs.run_command('v.what.rast', map_=newPts, raster=demNull, column='elev', layer=2)
+    gs.run_command('v.db.select', map_=newPts, layer=2, format_='csv', file=newElevFile, overwrite=True)
+
+
+ 
+
+
--- removeCulverts.py
+++ removeCulverts.py
@@ -5,71 +5,72 @@ Created on Wed May 21 11:03:19 2025
 @author: Uma
 """
 
-#%% Prerequisite modules, data, and folders
-
-import grass.script as gs
-import grass.grassdb.data as gdb
-
-# Folder names
-tmpFiles = 'tempFiles/'
-hucPrefix='testDEM3' # use for operations that need the DEM
-ditchPrefix='BRR' # use for operations involving the entire ditch layer
-
-# We need ditch vector data, culvert vector data, and elevation raster data
-ditchesCropped = hucPrefix + '_shiftedDitches'  # only covers extent of DEM
-dem = 'testDEM3'
-
-culvertBuffers = ditchPrefix + '_culvertBuffers'  # vector layer containing circles around the culvert points
-
-#%% Layers/files that will be created automatically
-culvertLines = hucPrefix + '_culvertLines'    # segment of ditch that passes through culvert
-culvertEndpts = hucPrefix + '_culvertEndpoints' 
-
-culvertMask = hucPrefix + '_culvertMask'
-
-culvertRaster = hucPrefix + '_culvertSurf'
-
-demBurned = hucPrefix + '_burned'
-demNull = hucPrefix + '_wNulls'
-
-newPts = hucPrefix + '_shiftedVertices'
-newElevFile = tmpFiles + hucPrefix + '_elevProfile_shiftedDitches.txt'
-       
-#%% Need DEM for this part
-
-if not gdb.map_exists(demNull, 'raster'):
-    gs.run_command('g.region', raster=dem)
-    
-    # Then find where these buffers intersect the ditch lines
-    gs.run_command('v.overlay', ainput=ditchesCropped, atype='line', binput=culvertBuffers, \
-                    operator='and', output=culvertLines)
-    
-    # First find the endpoints of each culvert segment
-    gs.run_command('v.to.points', input_=culvertLines, output=culvertEndpts, use='node', overwrite=True)
-    
-    # Also, we need a narrow mask (for burning drainage), only in regions where culverts are
-    gs.run_command('v.buffer', flags='c', input_=culvertLines, type_='line', output=culvertMask, \
-                    distance=3) 
-    # Above is a vector, but we need a raster mask
-    gs.run_command('v.to.rast', input_=culvertMask, type_='area', output=culvertMask, use='value')
+def removeCulverts(tmpFiles, layerPrefix, hucPrefix, \
+                   culvertBuffers, ditchLines, demForNull, demForBurn):
+    """ Takes two file prefixes: tmpFiles, layerPrefix, hucPrefix
+        Also takes GIS layer names
+            - culvertBuffers: vector areas showing radius around culverts
+            - ditchLines: vector lines showing ditches
+            - demForNull: raster to set areas to null
+            - demForBurn: raster to burn drainage into
+        Creates layer showing portions of ditches that pass through culverts,
+        interpolates surfaces in these areas, 
+        and replaces original raster with burned drainage
+        """
         
-    ### Create interpolated surfaces where the culvert regions are
-    # Get elevation values at endpoints of culvert segments
-    gs.run_command('v.what.rast', map_=culvertEndpts, raster=dem, column='elev', layer=2, overwrite=True)
-    # Interpolate a surface from these points
-    gs.run_command('v.surf.rst', input_=culvertEndpts, zcolumn='elev', \
-                    elevation=culvertRaster, mask=culvertMask, layer=2)
-    # Now patch the interpolated section with the original DEM,
-    # using the interpolated part as the primary raster
-    gs.run_command('r.patch', input_=[culvertRaster,dem], output=demBurned)
+    import grass.script as gs
+    import grass.grassdb.data as gdb
     
-    ### Create null regions where the culvert regions are
-    expr=demNull + '=if(isnull('+ culvertMask+ '),' + dem + ', 0)'
-    gs.run_command('r.mapcalc', expression=expr)
-    gs.run_command('r.null', map_=demNull, setnull=0)
+    # Define layer names first, will be created later
     
-    gs.run_command('v.to.points', input_=ditchesCropped, dmax=1, output=newPts)
-    gs.run_command('v.to.db', map_=newPts, layer=2, option='coor', columns=['x', 'y'])
+    culvertLines = layerPrefix + '_culvertLines' # segment of ditch that passes through culvert
+    culvertEndpts = layerPrefix + '_culvertEndpoints'
+    culvertMask = layerPrefix + '_culvertMask'
+    culvertRaster = layerPrefix + '_culvertSurf'
+     
+    demNull = layerPrefix + '_wNulls'
+    demBurned = layerPrefix + '_burned'
+    
+    # newPts has the hucPrefix because we only need to create it once 
+    # across multiple iterations
+    newPts = hucPrefix + '_shiftedVertices'
+    newElevFile = tmpFiles + layerPrefix + '_elevProfile_shiftedDitches.txt'
+    
+    if not gdb.map_exists(demBurned, 'raster'):
+        gs.run_command('g.region', raster=demForNull)
+        
+        # Find where buffers intersect the ditch lines
+        gs.run_command('v.overlay', ainput=ditchLines, atype='line', binput=culvertBuffers, \
+                        operator='and', output=culvertLines)
+
+        # First find the endpoints of each culvert segment
+        gs.run_command('v.to.points', input_=culvertLines, output=culvertEndpts, use='node', overwrite=True)
+    
+        # Also, we need a narrow mask (for burning drainage), only in regions where culverts are
+        gs.run_command('v.buffer', flags='c', input_=culvertLines, type_='line', output=culvertMask, \
+                        distance=3) 
+        # Above is a vector, but we need a raster mask
+        gs.run_command('v.to.rast', input_=culvertMask, type_='area', output=culvertMask, use='value')
+        
+        ### Create interpolated surfaces where the culvert regions are
+        # Get elevation values at endpoints of culvert segments
+        gs.run_command('v.what.rast', map_=culvertEndpts, raster=demForBurn, column='elev', layer=2, overwrite=True)
+        # Interpolate a surface from these points
+        gs.run_command('v.surf.rst', input_=culvertEndpts, zcolumn='elev', \
+                        elevation=culvertRaster, mask=culvertMask, layer=2)
+        # Now patch the interpolated section with the original DEM,
+        # using the interpolated part as the primary raster
+        gs.run_command('r.patch', input_=[culvertRaster,demForBurn], output=demBurned, overwrite=True)
+    
+        ### Create null regions where the culvert regions are
+        expr=demNull + '=if(isnull('+ culvertMask+ '),' + demForNull + ', 0)'
+        gs.run_command('r.mapcalc', expression=expr)
+        gs.run_command('r.null', map_=demNull, setnull=0)
+        
+    if not gdb.map_exists(newPts, 'vector'):
+        gs.run_command('v.to.points', input_=ditchLines, dmax=1, output=newPts)
+        gs.run_command('v.to.db', map_=newPts, layer=2, option='coor', columns=['x', 'y'])
+        
     gs.run_command('v.what.rast', map_=newPts, raster=demNull, column='elev', layer=2)
     gs.run_command('v.db.select', map_=newPts, layer=2, format_='csv', file=newElevFile, overwrite=True)
 
