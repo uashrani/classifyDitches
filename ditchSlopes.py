dc43e0f36b1a7588150cc9f53503cc6c9d559887
uashrani <ashra061@umn.edu>
2025-08-05 17:20:26 -0500

    split lines if they have two obvious segments with different flow directions

--- ditchSlopes.py
+++ ditchSlopes.py
@@ -15,28 +15,38 @@ import numpy as np
 
 import removeCulverts
 
-tmpFiles = 'tempFiles/'
-hucPrefix = 'testDEM2'
+tmpFiles = 'tempFiles2/'
+hucPrefix = 'testDEM5'
 ditchPrefix = 'BRR'
 
 chainFile = tmpFiles + ditchPrefix + '_streamChains.txt'
 newElevFile = tmpFiles + hucPrefix + '_elevProfile_shiftedDitches.txt'
 
-newLine = hucPrefix + '_shiftedDitches'
+definedLine = hucPrefix + '_shiftedDitches_notCleaned'
 
 demNull = hucPrefix + '_wNulls'
 demBurned = hucPrefix + '_burned'
 
+# For splitting lines with differing slopes
+vecLines3 = ditchPrefix + '_lines_rmdupl2'
+nodesFile = tmpFiles + ditchPrefix + '_nodesTemp.txt'
+
 #%% To be created
-vecLines6 = hucPrefix + '_lines_flowDir'
+vecLines7 = hucPrefix + '_lines_flowDirTemp'
 
 culvertDefFile = tmpFiles + hucPrefix + '_culvertPtDefs.txt'   # file that GRASS will read from 
 
 culvertPts = hucPrefix + '_culvertPoints'   # points layer of culvert locations
 culvertBuffers = hucPrefix + '_culvertBuffers'  # vector layer containing circles around the culvert points
 
+# Temp layers if we are splitting 
+tempSplit1 = hucPrefix + '_tempSplit1'
+splitPts = hucPrefix + '_splitPts'
+
+vecLines8 = hucPrefix + 'lines_flowDir'
+
 #%% Actual code   
-gs.run_command('g.region', vector=newLine)
+gs.run_command('g.region', vector=definedLine)
 
 chainDf = pd.read_csv(chainFile)   
 df = pd.read_csv(newElevFile)
@@ -47,22 +57,21 @@ unmappedCulverts = pd.DataFrame({'x': [], 'y': []})
 
 newChainDf = chainDf.copy()
 
-if not gdb.map_exists(vecLines6, 'vector'):
-    gs.run_command('g.copy', vector=[newLine, vecLines6])
+dropFids, dropCats = [], []
+
+if not gdb.map_exists(vecLines7, 'vector'):
+    gs.run_command('g.copy', vector=[definedLine, vecLines7])
 
     ### Do linear regression and flip vector directions if needed
     for lcat in lcats:
         
-        #print(lcat)
-        
         chain=[lcat]
     
-        thisDitch = df[df['lcat']==lcat]
-        filtProfile = thisDitch[np.isnan(thisDitch['elev'])==False]
-        along, elev = filtProfile['along'], filtProfile['elev']
+        thisDitch = df[(df['lcat']==lcat) & (np.isnan(df['elev'])==False)]
+        along, elev = thisDitch['along'], thisDitch['elev']
         
         # Try linear regression with just a single ditch segment first
-        if len(along) >=2: 
+        if len(along) >= 25: 
             linreg = sp.stats.linregress(along, elev)  
             r2=linreg.rvalue**2
         
@@ -77,20 +86,20 @@ if not gdb.map_exists(vecLines6, 'vector'):
                   strpChain)
                 
             for (j, segment) in enumerate(chain):
-                thisDitch = df[df['lcat']==segment]
+                chainDitch = df[(df['lcat']==segment) & (np.isnan(df['elev'])==False)]
                 if j==0:
-                    concatDf = thisDitch.reset_index(drop=True)
+                    concatDf = chainDitch.reset_index(drop=True)
                 else:
-                    if len(concatDf) > 0: thisDitch.loc[:, 'along']=thisDitch['along'] + concatDf['along'].iloc[-1]
-                    concatDf = pd.concat((concatDf, thisDitch)).reset_index(drop=True)
+                    if len(concatDf) > 0: chainDitch.loc[:, 'along']=chainDitch['along'] + concatDf['along'].iloc[-1]
+                    concatDf = pd.concat((concatDf, chainDitch)).reset_index(drop=True)
                     
-            filtProfile = concatDf[np.isnan(concatDf['elev'])==False]
-            along, elev = filtProfile['along'], filtProfile['elev']
+            thisDitch = concatDf
+            along, elev = thisDitch['along'], thisDitch['elev']
             
             linreg = sp.stats.linregress(along, elev)
             r2 = linreg.rvalue**2
             
-        x, y = filtProfile['x'], filtProfile['y']
+        x, y = thisDitch['x'], thisDitch['y']
         linElev = linreg.slope * along + linreg.intercept  
         
         # Calculate the absolute value of error 
@@ -105,7 +114,8 @@ if not gdb.map_exists(vecLines6, 'vector'):
         peakIndsEP = []
         
         if np.max(along) > 50:
-            start25, end25 = np.where(along>25)[0][0], np.where(along+25<along.iloc[-1])[0][-1]
+            start25 = np.where(along>25)[0][0]
+            end25 = np.where(along+25<along.iloc[-1])[0][-1]
             startSlope = (elev.iloc[start25] - elev.iloc[0]) / (along.iloc[start25] - along.iloc[0])
             endSlope = (elev.iloc[-1] - elev.iloc[end25]) / (along.iloc[-1] - along.iloc[end25]) 
             
@@ -126,34 +136,93 @@ if not gdb.map_exists(vecLines6, 'vector'):
             
         dropInds=pd.Series(dropInds)
         dropInds = dropInds[dropInds < len(elev)]
-            
-        filtElev=elev.drop(elev.index[dropInds])
-        filtAlong=along.drop(along.index[dropInds])
+        
+        filtDf = thisDitch.drop(thisDitch.index[dropInds])
+        filtAlong, filtElev = filtDf['along'], filtDf['elev']
         
         linreg = sp.stats.linregress(filtAlong, filtElev)
         r2 = linreg.rvalue**2
                 
         #print(lcat,linreg.slope)
             
-        if linreg.slope > 0:
-            gs.run_command('v.edit', map_=vecLines6, tool='flip', cats=lcat)
-            chain = chain[::-1]
-            newRoot = chain[0]
-            newChainDf.loc[lcat-1, 'chain']=str(chain)
+        if r2 >= 0.4 and linreg.slope > 0:
+            gs.run_command('v.edit', map_=vecLines7, tool='flip', cats=lcat)
         if r2 < 0.4:
-            print('Warning: Ditch ' + str(lcat) + ' still has r2 < 0.4 even after concatenating profiles.')
+            a,b,c = np.polyfit(filtAlong,filtElev,2)
+            polyMin = -b / (2*a)
+            
+            if polyMin > 0 and polyMin < np.max(filtAlong):
+                #filtDf = pd.DataFrame({'along': filtAlong, 'elev':filtElev})
+                ditch1 = filtDf[filtDf['along'] <= polyMin]
+                ditch2 = filtDf[filtDf['along'] > polyMin]
+                
+                linreg1 = sp.stats.linregress(ditch1['along'], ditch1['elev'])
+                linreg2 = sp.stats.linregress(ditch2['along'], ditch2['elev'])
+            
+                rsq1, rsq2 =linreg1.rvalue**2, linreg2.rvalue**2
+                slope1, slope2 = linreg1.slope, linreg2.slope
+                
+                if rsq1 >= 0.4 and rsq2 >= 0.4:
+                    distFromSplit = np.abs(filtAlong - polyMin)
+                    toSplit = filtDf[distFromSplit==min(distFromSplit)].iloc[0]
+                    
+                    midpoint1=ditch1.iloc[len(ditch1)//2]
+                    midpoint2=ditch2.iloc[len(ditch2)//2]
+                    
+                    x1, y1 = midpoint1['x'], midpoint1['y']
+                    x2, y2 = midpoint2['x'], midpoint2['y']
+                    
+                    midDf = pd.DataFrame({'x':[x1,x2], 'y':[y1,y2]})
+                    midDf.to_csv('splitMidpoints.txt', index=False, header=False)
+                    gs.run_command('v.in.ascii', input_='splitMidpoints.txt', output=splitPts, \
+                                   separator='comma', columns=['x double precision', 'y double precision'])
+                    gs.run_command('v.db.addtable', map_=splitPts)
+                    gs.run_command('v.db.addcolumn', map_=splitPts, columns='orig_cat int')
+                    gs.run_command('v.what.vect', map_=splitPts, column='orig_cat', query_map=vecLines3, query_column='cat', dmax=10)
+                    gs.run_command('v.db.select', map_=splitPts, format_='csv', file='split_origcats.txt', overwrite=True)
+                    
+                    d=pd.read_csv('split_origcats.txt')
+                    oc1, oc2 = d['orig_cat'].iloc[0], d['orig_cat'].iloc[1]
+                    # If this was originally two lines that shouldn't have been merged,
+                    # find the closest original node
+                    if oc1 != oc2:
+                        nodesDf=pd.read_csv(nodesFile)
+                        starts, ends = nodesDf.iloc[::3].reset_index(drop=True), \
+                            nodesDf.iloc[2::3].reset_index(drop=True)
+                        nodesDf = pd.concat((starts,ends), ignore_index=True)
+                        nodesDf=nodesDf[(nodesDf['lcat']==oc1)|(nodesDf['lcat']==oc2)]
+                        nodesDf.loc[:,'dist']=np.sqrt((nodesDf['x']-toSplit['x'])**2+(nodesDf['y']-toSplit['y'])**2)
+                        toSplit = nodesDf[nodesDf['dist']==min(nodesDf['dist'])].iloc[0]
+                    
+                    gs.run_command('v.edit', map_=vecLines7, tool='break', coords=[toSplit['x'],toSplit['y']], threshold=10)
+                    
+                    fid1=gs.read_command('v.edit', map_=vecLines7, tool='select', bbox=[x1-0.1,y1-0.1,x1+0.1,y1+0.1])
+                    fid1 = int(fid1.split('\n')[0])
+                    fid2=gs.read_command('v.edit', map_=vecLines7, tool='select', bbox=[x2-0.1,y2-0.1,x2+0.1,y2+0.1])
+                    fid2 = int(fid2.split('\n')[0])
+                    
+                    if slope1 > 0: gs.run_command('v.edit', map_=vecLines7, tool='flip', ids=fid1)
+                    if slope2 > 0: gs.run_command('v.edit', map_=vecLines7, tool='flip', ids=fid2)
+                    
+                    dropFids += [fid1]
+                    dropCats += [lcat]
+                    
+                else:
+                    print('Warning: Ditch ' + str(lcat) + ' still has r2 < 0.4 even after concatenating profiles.')
 
-    newChainDf.to_csv(chainFile, index=False)
+    gs.run_command('v.category', input_=vecLines7, cat=dropCats, ids=dropFids, option='del', output=tempSplit1)
+    gs.run_command('v.category', input_=tempSplit1, option='add', output=vecLines8)
+    
     unmappedCulverts.to_csv(culvertDefFile, index=False, header=False)
     
     # Create points layer with unmapped culvert locations
-    gs.run_command('v.in.ascii', input_=culvertDefFile, output=culvertPts, \
-                    separator='comma', columns=['x double precision', 'y double precision'])
+    # gs.run_command('v.in.ascii', input_=culvertDefFile, output=culvertPts, \
+    #                 separator='comma', columns=['x double precision', 'y double precision'])
         
-    # Buffer the culvert points
-    gs.run_command('v.buffer', input_=culvertPts, type_='point', \
-                    output=culvertBuffers, distance=25)
+    # # Buffer the culvert points
+    # gs.run_command('v.buffer', input_=culvertPts, type_='point', \
+    #                 output=culvertBuffers, distance=25)
     
 # Later make a mega program that calls all functions, but for now do it here
-removeCulverts.removeCulverts(tmpFiles, hucPrefix + '_v2', hucPrefix, \
-                              culvertBuffers, vecLines6, demNull, demBurned)
+# removeCulverts.removeCulverts(tmpFiles, hucPrefix + '_v2', hucPrefix, \
+#                               culvertBuffers, vecLines7, demNull, demBurned)
