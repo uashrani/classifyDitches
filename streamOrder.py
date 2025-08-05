d03f974eec068e026f586df1dc2e1a4d89449fe5
uashrani <ashra061@umn.edu>
2025-08-05 17:21:01 -0500

    hopefully not building polylines here

--- streamOrder.py
+++ streamOrder.py
@@ -9,18 +9,18 @@ import grass.grassdb.data as gdb
 import pandas as pd
 import networkx as nx
 
-tmpFiles = 'tempFiles/'
+tmpFiles = 'tempFiles2/'
 hucPrefix = 'testDEM3'
 ditchPrefix='BRR'
 
-vecLines6 = hucPrefix + '_lines_flowDir'
+vecLines8 = hucPrefix + '_lines_flowDir'
 
 # This is just to tell us what lcats are in the region
 newElevFile = tmpFiles + hucPrefix + '_elevProfile_shiftedDitches.txt'
 
 #%% To be created
 
-vecLines = hucPrefix + '_lines_final'
+#vecLines = hucPrefix + '_lines_final'
 
 startNodes = hucPrefix + '_starts'
 endNodes = hucPrefix + '_ends'
@@ -82,15 +82,15 @@ def findOrder(lcat, orderDf):
 #%% Find stream orders
 ### Build polylines and find where end of one segment flows into start of another
 if not gdb.map_exists(endNodes, 'vector'):
-    gs.run_command('v.build.polylines', input_=vecLines6, output=vecLines, cats='first', type_='line')
-    gs.run_command('v.db.droptable', flags='f', map_=vecLines)
-    gs.run_command('v.db.addtable', map_=vecLines)
-    gs.run_command('v.to.points', input_=vecLines, output=startNodes, use='start', overwrite=True)
-    gs.run_command('v.to.points', input_=vecLines, output=endNodes, use='end', overwrite=True)
+    #gs.run_command('v.build.polylines', input_=vecLines6, output=vecLines, cats='first', type_='line')
+    #gs.run_command('v.db.droptable', flags='f', map_=vecLines)
+    #gs.run_command('v.db.addtable', map_=vecLines)
+    gs.run_command('v.to.points', input_=vecLines8, output=startNodes, use='start', overwrite=True)
+    gs.run_command('v.to.points', input_=vecLines8, output=endNodes, use='end', overwrite=True)
     
     #gs.run_command('db.droptable', flags='f', table=connectTable)
     gs.run_command('v.distance', flags='a', from_=endNodes, to=startNodes, from_layer=1, to_layer=1, \
-                    dmax=0.1, upload='cat', table= connectTable, overwrite=True)
+                    dmax=10, upload=['cat','dist'], table= connectTable, overwrite=True)
     gs.run_command('db.select', table=connectTable, separator='comma', output=connectFile, overwrite=True)
 
 p = pd.read_csv(newElevFile)
@@ -102,44 +102,44 @@ connectDf = connectDf[connectDf['from_cat']!=connectDf['cat']]
 graph = nx.from_pandas_edgelist(connectDf, source='from_cat', target='cat', create_using=nx.DiGraph)
 
 # Go through all lines for upstream neighbors & stream order
-orderDf = pd.DataFrame({'cat': lcats})
+# orderDf = pd.DataFrame({'cat': lcats})
 
-orderDf['parents']=''
-orderDf['order']=0
-orderDf['braid']=''
+# orderDf['parents']=''
+# orderDf['order']=0
+# orderDf['braid']=''
 
-# First pass: note parents of each ditch
-for i in range(len(orderDf)):
-    lcat = orderDf['cat'].iloc[i]
+# # First pass: note parents of each ditch
+# for i in range(len(orderDf)):
+#     lcat = orderDf['cat'].iloc[i]
     
-    # Find upstream neighbors/parents
-    if graph.has_node(lcat):
-        parents = str(list(graph.predecessors(lcat)))
+#     # Find upstream neighbors/parents
+#     if graph.has_node(lcat):
+#         parents = str(list(graph.predecessors(lcat)))
         
-        sameParents = orderDf.index[orderDf['parents']==parents]
-        if len(sameParents) > 0 and parents != '[]':
-            orderDf.loc[i, 'braid'] = parents
-            orderDf.loc[sameParents, 'braid'] = parents
-    else:
-        parents = '[]'
+#         sameParents = orderDf.index[orderDf['parents']==parents]
+#         if len(sameParents) > 0 and parents != '[]':
+#             orderDf.loc[i, 'braid'] = parents
+#             orderDf.loc[sameParents, 'braid'] = parents
+#     else:
+#         parents = '[]'
         
-    orderDf.loc[i, 'parents'] = parents
+#     orderDf.loc[i, 'parents'] = parents
     
-orderDf.loc[orderDf['parents'] == '[]', 'order'] = 1
+# orderDf.loc[orderDf['parents'] == '[]', 'order'] = 1
 
-if not gdb.map_exists(ditchPrefix + '_order1', 'vector'):
-    for ordr in range(1,5):
-        mapName = ditchPrefix + '_order' + str(ordr)
-        gs.run_command('v.edit', map_=mapName, type_='line', tool='create', overwrite=True)
+# if not gdb.map_exists(ditchPrefix + '_order1', 'vector'):
+#     for ordr in range(1,5):
+#         mapName = ditchPrefix + '_order' + str(ordr)
+#         gs.run_command('v.edit', map_=mapName, type_='line', tool='create', overwrite=True)
 
-#gs.run_command('v.db.addcolumn', map_=vecLines, columns='order int')
+# #gs.run_command('v.db.addcolumn', map_=vecLines, columns='order int')
 
-# Next pass: 
-for lcat in lcats:
-    order, orderDf = findOrder(lcat, orderDf)
-    mapName = ditchPrefix + '_order' + str(order)
-    print(lcat, order)
-    gs.run_command('v.edit', map_=mapName, tool='copy', bgmap=vecLines, cat=lcat)
+# # Next pass: 
+# for lcat in lcats:
+#     order, orderDf = findOrder(lcat, orderDf)
+#     mapName = ditchPrefix + '_order' + str(order)
+#     print(lcat, order)
+    #gs.run_command('v.edit', map_=mapName, tool='copy', bgmap=vecLines, cat=lcat)
     
     #gs.run_command('v.db.update', map_=vecLines, column='order', value=order, where="cat = "+str(lcat))
 
